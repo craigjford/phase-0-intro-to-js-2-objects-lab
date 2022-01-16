c8a212207becabd8629e9dd6d80a02f715ea2f49
Craig Ford <craig_ford@icloud.com>
2022-01-16 18:49:26 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Craig Ford",
+    streetAddress: "1726 Frank St",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newEmp = {...employee};
+    delete newEmp[key];
+    return newEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete employee[key];
+    return employee;
+}
