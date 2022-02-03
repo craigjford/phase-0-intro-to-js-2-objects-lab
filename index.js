ead0925d588e53efd88ed535133d5684cbb2e139
Craig Ford <craig_ford@icloud.com>
2022-02-03 12:09:27 -0500

    commit tree 15e88142b99614696b3e48eb008f3effaca28956

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Write your solution in this file!
+/* Write your solution in this file!
 const employee = {
     name: "Craig Ford",
     streetAddress: "1726 Frank St",
@@ -25,3 +25,31 @@ function destructivelyDeleteFromEmployeeByKey(obj, key) {
     delete employee[key];
     return employee;
 }
+*/
+const employee = {
+    name: 'Craig Ford',
+    address: '1726 Frank St'
+} 
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    employee[key] = value;
+    return employee; 
+} 
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete employee[key];
+    return employee;
+}
+
